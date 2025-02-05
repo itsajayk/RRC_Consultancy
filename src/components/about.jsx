42e8fdc67899042c72bfdac0d862e9fc0c3f7de0
itsajayk <itsajayk@outlook.com>
2025-02-05 13:56:11 +0530

    Initial commit or update

---
+++ src/components/about.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <motion.div 
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ once: false, amount: 0.2 }}
+        className="row w-full p-10 bg-blue-500 text-white text-left rounded-lg tracking-wide"
+        >
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>About Us</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>Why Choose Us?</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
